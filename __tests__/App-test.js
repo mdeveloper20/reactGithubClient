023c77c97ba7e0851219a1e7f0b8eace1741baee
Masoud <masoud.pezeshkzade@gmail.com>
2019-10-04 14:39:31 +0330

    change tests.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import AppOld from '../App';
+//import AppOld from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
