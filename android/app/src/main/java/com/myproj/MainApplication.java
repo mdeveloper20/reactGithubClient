da23f35424266b5c6773883eb3819fd461eec23f
Masoud <masoud.pezeshkzade@gmail.com>
2019-10-05 06:26:41 +0330

    implementing firebase push notification service on project.

--- android/app/src/main/java/com/myproj/MainApplication.java
+++ android/app/src/main/java/com/myproj/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.microsoft.codepush.react.CodePush;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
